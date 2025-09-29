16b775cdb2ab7bebf2d9868859dd7bab449eef94
RALHI <ralhi.certification@gmail.com>
2025-09-30 04:58:17 +0530

    Define the static file configurations to render on templates

--- Ekart/settings.py
+++ Ekart/settings.py
@@ -116,6 +116,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = [
+    'Ekart/static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
